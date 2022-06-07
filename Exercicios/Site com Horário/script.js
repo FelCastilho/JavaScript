bb07a62a6db518fe2076f734bed88080bb4761c5
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-06-07 12:56:27 -0300

    Manutenção do exercicio
    Tentei arrumar o exercicio de imagens, porem não consegui alterar

--- Exercicios/Site com Horário/script.js
+++ Exercicios/Site com Horário/script.js
@@ -1,18 +1,14 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('foto')
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
     var data = new Date()
     var hora = data.getHours()
-    var minuto = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos`
-    if (hora >= 0 && hora < 12){
-        img.src = '<img src="fotomanha.png>'
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src='imagens/manha.png'
+    }   else if (hora >= 12 && hora <= 18) {
+        img.src='imagens/tarde.png'
+    }   else if (hora > 18 && hora <= 24) {
+        img.src='imagens/noite.png'
     }
-    else if (hora >= 12 && hora <= 18){
-        img.src = '<img src="fotomanha.png>'
-    }
-    else{
-        img.src = '<img src="fotomanha.png>'
-    }
-
 }
