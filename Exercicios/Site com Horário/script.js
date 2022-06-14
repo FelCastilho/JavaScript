b23dbd7dda3cbc9193bb58137a81ea84119177ef
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-06-14 20:59:02 -0300

    Problema imagens Resolvido!!!
    O problema das imagens não aparecerem ao colocar as condições foi resolvido, o JS só aceita dentro da pasta do próprio arquivo!

--- Exercicios/Site com Horário/script.js
+++ Exercicios/Site com Horário/script.js
@@ -1,14 +1,15 @@
 function carregar() {
-    var msg = window.document.getElementById('msg');
+    var msg = window.document.getElementById('mensagem');
     var img = window.document.getElementById('imagem');
     var data = new Date()
     var hora = data.getHours()
+    var minutos = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.src='imagens/manha.png'
+        img.src='manha.png'
     }   else if (hora >= 12 && hora <= 18) {
-        img.src='imagens/tarde.png'
+        img.src='tarde.png'
     }   else if (hora > 18 && hora <= 24) {
-        img.src='imagens/noite.png'
+        img.src='noite.png'
     }
 }
