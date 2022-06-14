f8164feb23c29619fb52434c0588b00ba5129a25
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-06-14 19:18:01 -0300

    adição de imagens

---
+++ Exercicios/Site que calcula a idade/script.js
@@ -0,0 +1,10 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano').value
+    var res = getElementById('res')
+
+    if(fano == null) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }
+    }
