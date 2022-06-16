6ef12ca8ab5b2b5f9bc6caf8ddc66fb20f3f70ca
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-06-16 10:45:40 -0300

    acrescentei os minutos

--- Exercicios/Site com Horário/script.js
+++ Exercicios/Site com Horário/script.js
@@ -4,7 +4,7 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
     if (hora >= 0 && hora < 12) {
         img.src='manha.png'
     }   else if (hora >= 12 && hora <= 18) {
