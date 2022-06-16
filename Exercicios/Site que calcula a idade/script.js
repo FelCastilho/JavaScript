4a74fbdf1ea8ea6bc7a5ad23ba7fc97fa75c6235
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-06-16 13:02:21 -0300

    alterei o local das imagens

--- Exercicios/Site que calcula a idade/script.js
+++ Exercicios/Site que calcula a idade/script.js
@@ -1,10 +1,13 @@
 function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano').value
-    var res = getElementById('res')
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
 
-    if(fano == null) {
+    if(fano.value == 0) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+       var fsex = document.getElementById('radsex')
+       var idade = ano - fano
     }
     }
