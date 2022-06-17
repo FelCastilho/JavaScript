3c854308a5bb049248bc51548079f80e4ecafb4c
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-06-17 15:31:38 -0300

    Update script.js

--- Exercicios/Site que calcula a idade/script.js
+++ Exercicios/Site que calcula a idade/script.js
@@ -8,6 +8,13 @@ function verificar(){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else{
        var fsex = document.getElementById('radsex')
-       var idade = ano - fano
+       var idade = ano - Number(fano.value)
+       var genero = ' '
+       if (fsex[0].checked){
+        genero = 'Homem'
+       } else if (fsex[1].checked){
+        genero = 'Mulher'
+       }
+       res.innerHTML = `Foi detectado ${genero} com ${idade} anos`
     }
     }
