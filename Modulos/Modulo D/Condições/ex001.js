8171ef36c27bb34d50b9c64ff9c7e7a4a7624919
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2022-10-08 15:10:26 -0300

    aplicando condições

---
+++ Modulos/Modulo D/Condições/ex001.js
@@ -0,0 +1,8 @@
+var vel = 3
+console.log(`A velocidade do seu carro é de ${vel} KM/HR!`)
+
+if(vel > 60.5){
+    console.log(`Você foi MULTADO por estar acima da via de 60 km/hr. Sua velocidade foi de ${vel}`)
+}else{
+    console.log('Dirija sempre com segurança!')
+}
