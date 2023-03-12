c24cc53401804d55ef7d46a032e2b9d4dfde8215
FelipeCasti <106931155+FelCastilho@users.noreply.github.com>
2023-03-12 10:28:12 -0300

    Avançando estudos de JavaScript

---
+++ CBF cursos/Condições/ex001.js
@@ -0,0 +1,16 @@
+let n1 = 51
+let n2 = 11
+
+//Condições
+if (n1 > 10 ){
+    console.log('Numeral maior que 10')
+    if(n1 > 50){
+        console.log('Numero maior que 50')
+    }
+}else if ( n1 > 5){
+    console.log('Numeral esta entre 6 e 10')
+}else{
+    console.log('Numeral menor ou igal a 5')
+}
+
+console.log('Fim do programa')
